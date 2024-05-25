b507b5a9ce506f385d28bbfa1d6ecf4e7b5ddaee
Dize dev <axiozweb2022@gmail.com>
2024-05-24 22:02:32 -0500

    se agreo funcionalidad con js para mostrar el menu y culatarlo

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 
 window.addEventListener('DOMContentLoaded', () => {
+
     document.querySelector('.icon-menu').addEventListener('click', () => {
 
         document.querySelector('.sidebar').classList.toggle('active');
 
+        document.querySelectorAll('.sidebar-links a').forEach(btn => {
+
+            btn.addEventListener('click', event => {
+
+                event.preventDefault();
+                document.querySelector('.selected').classList.remove('selected');
+                btn.classList.add('selected')
+            });
+        });
     });
 });
 
